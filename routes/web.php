a6db67b101ec1ba7556e2fb01893c4e5deb0b6f4
Rémi TREVISANUTO <remi.trevisanuto@laposte.net>
2025-04-17 12:10:31 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,23 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-Route::get('/upload-test', function () {
-    return '
-        <form method="POST" action="/upload-test" enctype="multipart/form-data">
-            '.csrf_field().'
-            <input type="file" name="image" />
-            <button type="submit">Uploader</button>
-        </form>
-    ';
-});
-
 Route::post('/upload-test', function (Request $request) {
-    $path = $request->file('image')->store('test', 's3');
+    if (!$request->hasFile('image')) {
+        return 'Aucun fichier reçu.';
+    }
+
+    $file = $request->file('image');
+
+    if (!$file->isValid()) {
+        return 'Le fichier n\'est pas valide.';
+    }
+
+    $path = $file->store('test', 's3');
+
+    if (!$path) {
+        return 'Échec de l\'upload.';
+    }
+
     $url = Storage::disk('s3')->url($path);
     return "Image uploadée ! <br><a href='$url' target='_blank'>$url</a>";
 });
-
