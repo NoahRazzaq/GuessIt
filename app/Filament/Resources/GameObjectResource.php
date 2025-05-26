6e4e89fcafed07a514e585f5c8a17128579b185f
noahrazzaq <noahrazzaq@live.fr>
2025-05-26 09:24:21 +0200

    WIP (admin panel): creating admin panel

---
+++ app/Filament/Resources/GameObjectResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GameObjectResource\Pages;
+use App\Filament\Resources\GameObjectResource\RelationManagers;
+use App\Models\GameObject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GameObjectResource extends Resource
+{
+    protected static ?string $model = GameObject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGameObjects::route('/'),
+            'create' => Pages\CreateGameObject::route('/create'),
+            'edit' => Pages\EditGameObject::route('/{record}/edit'),
+        ];
+    }
+}
